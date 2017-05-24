6f98fb5347c06aecbbc21241a85aa421403bf55d
Karl <Oldwedge@users.noreply.github.com>
2017-05-24 16:15:31 +0200

    x
    x

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,20 +3,76 @@
 
 using namespace std;
 
+int main();
+
+void PrintIntro();
+void PlayGame();
+string GetGuess();
+bool AskToPlayAgain();
+
+//Entry point
 int main() 
 {
-	//introduce the game
+	PrintIntro();
+	PlayGame();
+	AskToPlayAgain();
+
+	return 0;
+}
+
+//introduce the game
+void PrintIntro() 
+{
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to the Bulls and Cows Game!\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram?\n";
+	return;
+}
 
-	//get guess from user
+//run the game core
+void PlayGame()
+{
+	
+	//constexpr to control guess limit of the game
+	constexpr int GUESS_LIMIT = 5;
+
+	//loop a number of turns to get guesses and print back
+	for (int counter = 1; counter <= GUESS_LIMIT; counter++)
+	{
+		cout << "What is your guess? ";
+		cout << "Your guess was: " << GetGuess() << endl;
+		cout << endl;
+	}
+	return;
+}
+
+//get guess from user
+string GetGuess() 
+{
 	string Guess = "";
-	cout << "What is your guess? ";
-	cin >> Guess;
-	//repeat
-	cout << "Your guess was " << Guess << endl;
+	getline(cin, Guess);
+	return Guess;
+}
 
-	return 0;
+bool AskToPlayAgain()
+{
+	string Response = "";
+	cout << "Do you want to play again? (y/n) ";
+	getline(cin, Response);
+	if ((Response[0] == 'Y') || (Response[0] == 'y'))
+	{
+		cout << "\n returns True";
+		return true;
+	}
+	else if ((Response[0] == 'N') || (Response[0] == 'n'))
+	{
+		cout << "\n returns False";
+		return false;
+	}
+	else 
+	{
+		cout << "Wrong input! Exitting...";
+		return false;
+	}	
 }
